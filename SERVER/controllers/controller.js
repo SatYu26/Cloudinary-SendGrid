08eca2cb36661368b0b41b76d08f653d321fa3a4
SatYu26 <goyalsatyam8@gmail.com>
2021-03-10 02:04:06 +0530

    sending email finish

--- SERVER/controllers/controller.js
+++ SERVER/controllers/controller.js
@@ -3,7 +3,43 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 emailFeedback = (req, res) => {
-  console.log(req.body);
+  const { name, email, message, phone, uploadedFiles } = req.body;
+
+  const emailData = {
+    to: process.env.EMAIL_TO,
+    from: email,
+    subject: "Feedback form",
+    html: `
+    <h1>Customer Feedback Form</h1>
+    <hr />
+    <h2>Sender Name: ${name}</h2>
+    <h2>Sender email: ${email}</h2>
+    <h2>Sender Phone: ${phone}</h2>
+    <h2>Sender Message: ${message}</h2>
+    <br />
+        ${uploadedFiles.map((file) => {
+          return `<img src="${file.secure_url}" alt="${file.original_filename}" style="width:50%; overflow:hidden; padding:50px;" />`;
+        })}
+
+    <hr />
+    <p>https://satyamgoyal.codes</p>
+    `,
+  };
+
+  sgMail
+    .send(emailData)
+    .then((sent) => {
+      console.log(sent);
+      return res.json({
+        success: true,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.json({
+        success: false,
+      });
+    });
 };
 
 module.exports = { emailFeedback };
