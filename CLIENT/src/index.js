e57c6fe1c64a6ef6d13800024259ada68cd34562
SatYu26 <goyalsatyam8@gmail.com>
2021-03-10 03:39:47 +0530

    SSR snapshot

--- CLIENT/src/index.js
+++ CLIENT/src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { render } from "react-snapshot";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
